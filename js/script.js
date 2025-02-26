8a9e4e38c99f7ff0cf4716dae2230257c6f7adc3
Ridwan4D <ridwanul161@gmail.com>
2025-02-26 18:52:53 +0600

    17px extra added with vartical line using js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", () => {
     const verticalLines = document.querySelectorAll(".hero_vertical_line_1");
 
     if (topSection && verticalLines.length > 0) {
-        const sectionHeight = topSection.offsetHeight;
+        const sectionHeight = topSection.offsetHeight + 17;
 
         verticalLines.forEach(line => {
             line.style.height = `${sectionHeight}px`;
@@ -16,7 +16,7 @@ window.addEventListener("resize", () => {
     const verticalLines = document.querySelectorAll(".hero_vertical_line_1");
 
     if (topSection && verticalLines.length > 0) {
-        const sectionHeight = topSection.offsetHeight;
+        const sectionHeight = topSection.offsetHeight + 17;
 
         verticalLines.forEach(line => {
             line.style.height = `${sectionHeight}px`;
