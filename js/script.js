ed7de83bd5a3dfe97540735960ecafa450832bc1
Ridwan4D <ridwanul161@gmail.com>
2025-02-27 00:41:20 +0600

    obser counter created

--- js/script.js
+++ js/script.js
@@ -1,25 +1,63 @@
+const counts = document.querySelectorAll(".observe_infos");
+const speed = 100;
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const counter = entry.target;
+
+        function updateData() {
+          const target = Number(counter.getAttribute("data-target"));
+          let count = 0;
+          const increment = target / speed;
+
+          counter.innerText = "0 +";
+
+          const interval = setInterval(() => {
+            count += increment;
+            if (count >= target) {
+              counter.innerText = target;
+              clearInterval(interval);
+            } else {
+              counter.innerText = Math.floor(count);
+            }
+          }, 8);
+        }
+
+        updateData();
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+counts.forEach((counter) => {
+  observer.observe(counter);
+});
+
 window.addEventListener("load", () => {
-    const topSection = document.getElementById("top_section");
-    const verticalLines = document.querySelectorAll(".hero_vertical_line_1");
+  const topSection = document.getElementById("top_section");
+  const verticalLines = document.querySelectorAll(".hero_vertical_line_1");
 
-    if (topSection && verticalLines.length > 0) {
-        const sectionHeight = topSection.offsetHeight + 17;
+  if (topSection && verticalLines.length > 0) {
+    const sectionHeight = topSection.offsetHeight + 17;
 
-        verticalLines.forEach(line => {
-            line.style.height = `${sectionHeight}px`;
-        });
-    }
+    verticalLines.forEach((line) => {
+      line.style.height = `${sectionHeight}px`;
+    });
+  }
 });
 
 window.addEventListener("resize", () => {
-    const topSection = document.getElementById("top_section");
-    const verticalLines = document.querySelectorAll(".hero_vertical_line_1");
+  const topSection = document.getElementById("top_section");
+  const verticalLines = document.querySelectorAll(".hero_vertical_line_1");
 
-    if (topSection && verticalLines.length > 0) {
-        const sectionHeight = topSection.offsetHeight + 17;
+  if (topSection && verticalLines.length > 0) {
+    const sectionHeight = topSection.offsetHeight + 17;
 
-        verticalLines.forEach(line => {
-            line.style.height = `${sectionHeight}px`;
-        });
-    }
+    verticalLines.forEach((line) => {
+      line.style.height = `${sectionHeight}px`;
+    });
+  }
 });
