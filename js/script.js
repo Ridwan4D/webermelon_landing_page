b61421c7882660aa0ab4e01a6069131b1b77f1e3
Ridwan4D <ridwanul161@gmail.com>
2025-03-02 15:43:40 +0600

    navbar position update

--- js/script.js
+++ js/script.js
@@ -80,3 +80,27 @@ new Swiper(".mySwiper", {
         }
     }
 });
+
+ document.addEventListener("DOMContentLoaded", function () {
+        const navbarToggler = document.getElementById("navbarToggler");
+        const navbarCollapse = document.getElementById("navbarSupportedContent");
+        const icon = navbarToggler.querySelector("span");
+
+        // Define the close (X) icon using SVG
+        const closeIcon = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+            </svg>`;
+
+        const defaultIcon = `<span class="navbar-toggler-icon"></span>`; // Bootstrap default icon
+
+        navbarToggler.addEventListener("click", function () {
+            setTimeout(() => { // Wait for Bootstrap animation
+                if (navbarCollapse.classList.contains("show")) {
+                    icon.innerHTML = closeIcon; // Show "X" when open
+                } else {
+                    icon.innerHTML = defaultIcon; // Show default icon when closed
+                }
+            }, 200);
+        });
+    });
